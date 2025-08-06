920ade2bcdacb14b6a39f076993c8afba9101315
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-08-06 21:52:52 +0300

    Update SECRET_KEY

--- rentr/settings.py
+++ rentr/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w_s963_my1q^x)dvc&(u$%c%i^1(!d(e(@*&z+5etsi*=a+9p1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
