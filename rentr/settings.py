f75166b23e3ce13a2be0683fe953f2563efbc6f4
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-08-06 21:53:50 +0300

    Remove remove comments

--- rentr/settings.py
+++ rentr/settings.py
@@ -6,9 +6,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
